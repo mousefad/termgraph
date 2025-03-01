84dd556e67019f2bcd47c64693a9cfda6dddf3e9
Mouse <mousefad@googlemail.com>
2025-03-01 18:53:46 +0000

    improve help text consistency.

--- termgraph/termgraph.py
+++ termgraph/termgraph.py
@@ -76,14 +76,14 @@ def init_args() -> Dict:
     parser = argparse.ArgumentParser(description="draw basic graphs on terminal")
     parser.add_argument("--bins", default=5, type=int, help="Bins of Histogram")
     parser.add_argument(
-        "--calendar", action="store_true", help="Calendar Heatmap chart"
+        "--calendar", action="store_true", help="Calendar Heatmap chart."
     )
-    parser.add_argument("--color", nargs="*", help="Graph bar color( s )")
+    parser.add_argument("--color", nargs="*", help="Graph bar color(s).")
     parser.add_argument(
-        "--custom-tick", default="", help="Custom tick mark, emoji approved"
+        "--custom-tick", default="", help="Custom tick mark, emoji approved."
     )
     parser.add_argument(
-        "--delim", default="", help="Custom delimiter, default , or space"
+        "--delim", default="", help="Custom delimiter, default , or space."
     )
     parser.add_argument(
         "--differentscale",
@@ -91,39 +91,39 @@ def init_args() -> Dict:
         help="Categories have different scales.",
     )
     parser.add_argument("--format", default="{:<5.2f}", help="format specifier to use.")
-    parser.add_argument("--histogram", action="store_true", help="Histogram")
+    parser.add_argument("--histogram", action="store_true", help="Histogram mode.")
     parser.add_argument(
         "--label-before",
         action="store_true",
         default=False,
-        help="Display the values before the bars",
+        help="Display the values before the bars.",
     )
     parser.add_argument(
-        "--no-labels", action="store_true", help="Do not print the label column"
+        "--no-labels", action="store_true", help="Do not print the label column."
     )
     parser.add_argument(
-        "--no-values", action="store_true", help="Do not print the values at end"
+        "--no-values", action="store_true", help="Do not print the values at end."
     )
     parser.add_argument(
         "--space-between",
         action="store_true",
         help="Print a new line after every field",
     )
-    parser.add_argument("--stacked", action="store_true", help="Stacked bar graph")
-    parser.add_argument("--start-dt", help="Start date for Calendar chart")
+    parser.add_argument("--stacked", action="store_true", help="Stacked bar graph.")
+    parser.add_argument("--start-dt", help="Start date for Calendar chart.")
     parser.add_argument(
         "--suffix", default="", help="string to add as a suffix to all data points."
     )
-    parser.add_argument("--title", help="Title of graph")
+    parser.add_argument("--title", help="Title of graph.")
     parser.add_argument(
-        "--verbose", action="store_true", help="Verbose output, helpful for debugging"
+        "--verbose", action="store_true", help="Verbose output, helpful for debugging."
     )
     parser.add_argument(
-        "--version", action="store_true", help="Display version and exit"
+        "--version", action="store_true", help="Display version and exit."
     )
-    parser.add_argument("--vertical", action="store_true", help="Vertical graph")
+    parser.add_argument("--vertical", action="store_true", help="Vertical graph mode.")
     parser.add_argument(
-        "--width", type=int, default=50, help="width of graph in characters default:50"
+        "--width", type=int, default=50, help="width of graph in characters default:50."
     )
     parser.add_argument(
         "filename",
